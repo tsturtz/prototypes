269efc51e768a72065039fb981de4f37ce92e7e0
Taylor Sturtz <taylorsturtz@gmail.com>
2016-11-03 11:14:18 -0700

    just started - files set up

---
+++ php_includes/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taylo
+ * Date: 11/3/2016
+ * Time: 11:06 AM
+ */
+$a = 1;
+$b = 5;
+include('includes/data.php');
+$result = $a * $b;
+print("<br>Result is $result");
+?>
