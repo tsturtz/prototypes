d7d683ca9e17acf4b04c8dfc32db54161db730c5
Taylor Sturtz <taylorsturtz@gmail.com>
2016-11-03 15:47:45 -0700

    Angular Services Intro - Taylor Sturtz

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -5,20 +5,28 @@ app.controller('myTunes', function ($http, $log) {
     this.createURL = function () {
         this.split_search = this.search_text.split(' ');
         this.search_term = this.split_search.join('+');
+        $log.log('search term is :', this.search_term);
+        this.hitUpDatabase();
+    };
+
+    this.hitUpDatabase = function () {
         var self = this;
         $http({
-            url: 'https://itunes.apple.com/search?term=' + search_term + '&callback=JSON_CALLBACK',
+            url: 'https://itunes.apple.com/search?term=' + self.search_term + '&callback=JSON_CALLBACK',
             method: 'jsonp',
             cache: false
         })
-        .then(
-            function (response) {
-                $log.log('successful search!');
-                //self.data = response.data.feed.entry
-            },
-            function (response) {
-                $log.log('failure to launch!');
-                //self.data = response.data || 'Failure to launch';
-            })
+            .then(
+                function (response) {
+                    $log.log('successful search!');
+                    $log.log(response);
+                    self.data = response.data.results;
+                    $log.log(self.data);
+                },
+                function (response) {
+                    $log.log('failure to launch!');
+                    self.data = response.data || 'Failure to launch';
+                })
     };
+
 });
