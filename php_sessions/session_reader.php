1958a8e2559c54dbbcf8d730b2f9e28790f9ba39
Taylor Sturtz <taylorsturtz@gmail.com>
2016-11-04 16:47:23 -0700

    php_sessions half-way through - Feature set 2 todo

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,1 +1,14 @@
 <!-- Session Reader -->
+<?php
+session_start();
+?>
+<pre>
+<?php
+print_r($_GET);
+$_SESSION['name'] = $_GET['name'];
+$_SESSION['age'] = $_GET['age'];
+$_SESSION['work'] = $_GET['work'];
+//print_r($_SESSION);
+//session_destroy();
+?>
+</pre>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,1 +1,32 @@
 <!-- Session Setter -->
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Session Setter</title>
+</head>
+<body>
+
+<form method="get" action="session_reader.php">
+
+    <input name="name" placeholder="Name"
+           value="<?= $name = isset($_SESSION['name']) ? $_SESSION['name'] : '' ?>">
+
+    <input name="age" placeholder="Age"
+           value="<?= $age = isset($_SESSION['age']) ? $_SESSION['age'] : '' ?>">
+
+    <input name="work" placeholder="Occupation"
+           value="<?= $work = isset($_SESSION['work']) ? $_SESSION['work'] : '' ?>">
+
+    <button type="submit">Submit</button>
+</form>
+<pre>
+<?php
+print_r($_SESSION);
+?>
+</pre>
+</body>
+</html>
