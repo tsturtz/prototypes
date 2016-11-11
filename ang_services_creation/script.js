9217b76b69d16401119bf57c8afebfeef15de362
Taylor Sturtz <taylorsturtz@gmail.com>
2016-11-10 19:19:45 -0800

    Angular Services Creation - Taylor Sturtz

--- ang_services_creation/script.js
+++ ang_services_creation/script.js
@@ -1,36 +1,35 @@
 var app = angular.module('sgtApp', []);
 
-app.provider('sgtData', function($get){
+app.provider('sgtData', function(){
     //Create a variable to hold this
-    var sgtDataThis = this;
+    var sgtDataSelf = this;
     //Create a variable to hold your api key but set it to an empty string
-    this.api_key = '';
+    sgtDataSelf.api_key = '';
     //Create a variable to hold the API url but set it to an empty string
-    this.api_url = '';
+    sgtDataSelf.api_url = '';
 
     //Add the necessary services to the function parameter list
-    this.$get = function($http,$q,$log){
+    sgtDataSelf.$get = function($http,$q,$log){
         //return an object that contains a function to call the API and get the student data
         //Refer to the prototype instructions for more help
         return {
             call_api: function () {
-                var data = 'api_key=' + api_key;
+                var data = 'api_key=' + sgtDataSelf.api_key;
                 var defer = $q.defer();
                 $http({
-                    url: api_url,
+                    url: sgtDataSelf.api_url,
                     method: 'post',
                     data: data,
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                }).then(function success(response){
-                    $log.log('success: ', data);
-                    defer.resolve(data);
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(function (response){
+                    $log.log('success: ', response);
+                    defer.resolve(response);
                 },
-                function error(response){
-                    $log.log('error: ', data);
+                function (response){
+                    $log.log('error: ', response);
                     defer.reject('Error message!');
                 });
+                $log.log('defer: ', defer.promise);
                 return defer.promise;
             }
         }
@@ -38,16 +37,28 @@ app.provider('sgtData', function($get){
 });
 
 //Config your provider here to set the api_key and the api_url
-
+app.config(function(sgtDataProvider){
+    sgtDataProvider.api_key = 'BmjoMo3MLu';
+    sgtDataProvider.api_url = 'https://s-apis.learningfuze.com/sgt/get';
+});
 
 //Include your service in the function parameter list along with any other services you may want to use
-app.controller('ioController', function(){
+app.controller('ioController', function(sgtData,$log){
     //Create a variable to hold this, DO NOT use the same name you used in your provider
-
+    var ioContSelf = this;
     //Add an empty data object to your controller, make sure to call it 'data'
-
-
+    ioContSelf.data = {};
     //Add a function called getData to your controller to call the SGT API
+    ioContSelf.getData = function () {
+        sgtData.call_api()
+            .then(function (response){
+                $log.log('success!');
+                ioContSelf.data = response.data;
+            },
+            function (response) {
+                $log.log('fail!!!');
+            })
+    };
     //Refer to the prototype instructions for more help
 
 });
