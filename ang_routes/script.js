de189fc6a7a7567da8fe9100e68ec705f79571e9
Taylor Sturtz <taylorsturtz@gmail.com>
2016-11-11 14:54:48 -0800

    Angular Routes - Taylor Sturtz

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,22 +1,46 @@
 // Create the route module and name it routeApp
-
+var app = angular.module('routeApp',['ngRoute']);
 // Config the routes
-
-         // route for the home page
-
+app.config(function($routeProvider){
+    $routeProvider
+        // route for the home page
+        .when('/home',{
+            templateUrl: 'pages/home.html',
+            controller: 'mainCtrl'
+        })
         // route for the about page
-
+        .when('/about',{
+            templateUrl: 'pages/about.html',
+            controller: 'aboutCtrl'
+        })
         // route for the contact page
-
+        .when('/contact',{
+            templateUrl: 'pages/contact.html',
+            controller: 'contactCtrl'
+        })
+        .otherwise({redirectTo: 'pages/home.html'});
+});
 
 // Create the controllers for the different pages below
+app
+    .controller('routeCtrl', function($scope){
+    //$scope.message = 'lorem ipsum 1';
+    })
 
 // home page controller
-    // Create a message to display in the view
 
+    .controller('mainCtrl', function($scope){
+    $scope.message = 'lorem ipsum HOME';
+    })
 
 // about page controller
-    // Create a message to display in the view
+
+    .controller('aboutCtrl', function($scope){
+    $scope.message = 'lorem ipsum ABOUT';
+    })
 
 // contact page controller
-    // Create a message to display in the view
+
+    .controller('contactCtrl', function($scope){
+    $scope.message = 'lorem ipsum CONTACT';
+    });
